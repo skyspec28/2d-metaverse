28824ac20c963c5ea0e5de23d8f11de2020c9252
skyspec28 <maulomepelumi@gmail.com>
2025-01-30 17:35:15 +0100

    added Element creation view

--- src/api/views.py
+++ src/api/views.py
@@ -1,20 +1,28 @@
-from django.shortcuts import render
-import requests
-import json
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework import status ,generics
-from api.models import Avatar ,Space ,SpaceElement
+from api.models import Avatar ,Space ,Element ,SpaceElement
+from rest_framework.permissions import IsAuthenticated ,IsAdminUser
 
 from api.serializers import (
     AvatarsSerializer,
     SpaceSerializer,
-    SpaceElementSerializer,
+    ElementSerializer
+    # SpaceElementSerializer,
     # ElementSerializer,
     # MapSerializeer,
     # MapELementSerializer,
     
 )
+
+"""
+Global Authentication Enforced Across All API Views
+
+To ensure that all API views in this project require authentication, we've configured the global permission 
+class in the Django REST Framework settings:
+The setting DEFAULT_PERMISSION_CLASSES in settings.py now includes IsAuthenticated.
+"""
+
+
 
 class AvatarRetrieveView(generics.RetrieveAPIView):
     queryset = Avatar.objects.all()
@@ -24,6 +32,7 @@ class AvatarRetrieveView(generics.RetrieveAPIView):
 class CreateSpaceAPIView(generics.CreateAPIView):
     queryset = Space.objects.all()
     serializer_class = SpaceSerializer
+    permission_classes = [IsAuthenticated] # only authenticated users can create spaces
     
     def create(self, request, format=None):
         serializer = self.get_serializer(data=request.data)
@@ -63,10 +72,53 @@ class SpaceListAPIView(generics.ListAPIView):
     serializer_class=SpaceSerializer
 
   
-class RetrieveSpaceElementAPIView(generics.RetrieveAPIView):
-    queryset=SpaceElement.objects.all()
-    serializer_class=SpaceElementSerializer
+# class RetrieveSpaceElementAPIView(generics.RetrieveAPIView):
+#     queryset=SpaceElement.objects.all()
+#     serializer_class=SpaceElementSerializer
 
-# class MapView(APIView):
-#     def get(self, request, map_id, format=None):
-                
+#  ADMIN ONLY TASK
+# ONLY ADMIN CAN ADD NEW MAP ,AVATARS ,STATTIC OBJECTS ,ELEMENTS ,
+# CAN CHANGE DIMENSIONS WHEN ADDED 
+# 
+
+# only admins can add and delete element to a map 
+
+class ElementCreateAPIView(generics.CreateAPIView):
+    """Create a new element , element are static objects like chairs """
+
+    queryset=Element.objects.all()
+    serializer_class=ElementSerializer
+    permission_classes = [IsAdminUser]
+
+    def create(self, request, format=None):
+        serializer= self.get_serializer(data=request.data)
+
+        if serializer.is_valid():
+            try:
+                element=serializer.save()
+                return Response(
+                    {
+                        "element_id":element.id
+                     
+                     },
+                    status=status.HTTP_201_CREATED
+                )
+
+            except Element.DoesNotExist:
+                return Response({"detail": "Element not found"}, status=status.HTTP_404_NOT_FOUND)
+            
+class ElementUpdateAPIView(generics.UpdateAPIView):
+    """Update an existing element"""
+    queryset=Element.objects.all()
+    serializer_class=ElementSerializer
+
+    def put (self, request, pk, format=None):
+        try :
+            element=Element.objects.get(id=pk)
+            serializer=self.get_serializer(element, data=request.data)
+        except Element.DoesNotExist:
+            return Response({"detail": "Element not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        if serializer.is_valid():
+            element_obj=serializer.save()
+
