d9e75e97dd67772e7c8b6742464aae5fb003e7fe
skyspec28 <maulomepelumi@gmail.com>
2025-01-31 20:05:21 +0100

    added map element

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from api.models import Avatar ,CustomUser ,Space
+from api.models import Avatar ,CustomUser ,Space ,UserMetadata ,Map , Element , SpaceElement ,MapElement 
 # Register your models here.
 
 admin.site.register(Avatar )
 admin.site.register(CustomUser)
 admin.site.register(Space)
+admin.site.register(UserMetadata)
+admin.site.register(Map)
+admin.site.register(Element)
+admin.site.register(SpaceElement)
+admin.site.register(MapElement)
