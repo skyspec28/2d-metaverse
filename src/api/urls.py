0acdde812500698e14d4480c205504c3c522f692
skyspec28 <maulomepelumi@gmail.com>
2025-03-05 19:24:37 +0100

    ♻️ refactor: update urls.py for better readability

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from .views import AvatarRetrieveView
 
 
 urlpatterns =[
-    path('avatars/<int:pk>/', views.AvatarRetrieveView.as_view(), name='avatar-detail'),
+    path('avatars/<int:pk>/', AvatarRetrieveView, name='avatar-detail'),
     path('spaces/new/', views.CreateSpaceAPIView.as_view(), name='space-create'),
     path('spaces/delete/<int:pk>/', views.DestroySpaceView.as_view(), name='space-delete'),
     path('spaces/all/', views.SpaceListAPIView.as_view(), name='space-list'),
